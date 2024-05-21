133adb27d9e6523e4a52d4866ce46ea10dc8c141
aijazmahdavi <aijazmahdavi@gmail.com>
2024-05-21 21:34:06 +0500

    Initial Commit, user auth Implement JWT

---
+++ api/views.py
@@ -0,0 +1,22 @@
+from django.contrib.auth import authenticate
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            refresh = RefreshToken.for_user(user)
+            access_token = str(refresh.access_token)
+            refresh_token = str(refresh)
+
+            return Response({
+                'access_token': access_token,
+                'refresh_token': refresh_token
+            })
+        else:
+            return Response({'error': 'Invalid credentials'}, status=401)
